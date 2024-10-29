137141394e705c43eae13e9602c6cc1141a4be8b
eziors <marcos.barbosa@mail.uft.edu.br>
2024-10-29 02:45:53 -0300

    Fixing repo issues22

---
+++ Utilities/UIHelper.swift
@@ -0,0 +1,40 @@
+//
+//  UIHelper.swift
+//  QuizFrench
+//
+//  Created by Marcos Barbosa on 29/10/24.
+//
+
+import UIKit
+
+enum CollectionType {
+    case alphabet, category
+}
+
+enum CategoryType {
+    case phrases, words
+}
+
+struct UIHelper {
+    static func createTwoColumnFlowLayout(in view: UIView, for collectionType: CollectionType) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let itemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (itemSpacing * 2)
+        let itemWidth = availableWidth / 2
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        
+        switch collectionType {
+        case .alphabet:
+            flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 20)
+
+        case .category:
+            flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 60)
+
+        }
+        return flowLayout
+    }
+}
+
